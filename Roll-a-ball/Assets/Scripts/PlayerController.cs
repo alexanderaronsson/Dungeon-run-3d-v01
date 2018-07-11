1554328996237b267817038f7ee5f7a181fab6e7
Alexander Aronsson <aronssonalexander@gmail.com>
2018-07-11 16:45:19 +0200

    Added project files
    Added project files

---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private int score;
+    //public UICharInfo scoreText;
+    public Text scoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        score = 0;
+        //Ger null reference då text inte fungerar som det ska
+        //scoreText.text = "Score " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveH, 0.0f, moveV);
+
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "pickup")
+        {
+            other.gameObject.SetActive(false);
+            //Ger null reference då text inte fungerar som det ska
+            //score++;
+            //scoreText.text = "Score " + score;
+
+        }
+    }
+
+}
